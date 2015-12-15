fa1812f240bba7f074743465d4fe0bc42071dfdd
thisisaaroland <aaron@mapzen.com>
2015-12-15 11:47:23 -0800

    update new sources; run compile in strict mode

--- bin/compile.py
+++ bin/compile.py
@@ -17,6 +17,11 @@ if __name__ == '__main__':
     sources = os.path.join(root, 'sources')
     spec = {}
 
+    prefixes = {}
+    names = {}
+
+    required = ("id", "name", "prefix")
+
     for (root, dirs, files) in os.walk(sources):
 
         for f in files:
@@ -32,6 +37,23 @@ if __name__ == '__main__':
             except Exception, e:
                 logging.error("failed to parse %s, because %s" % (path, e))
                 sys.exit()
+
+            for k in required:
+                if not data.get(k, False):
+                    logging.error("%s is missing a %s key" % (path, k))
+                    sys.exit()
+
+            name = data['name']
+            prefix = data['prefix']
+
+            if name in names:
+                logging.error("%s is trying to claim name '%s' (already assigned to %s)" % (path, name, names[name]))
+
+            if prefix in prefixes:
+                logging.error("%s is trying to claim name '%s' (already assigned to %s)" % (path, prefix, prefixes[prefix]))
+
+            names[ name ] = path
+            prefixes[ prefix ] = path
 
             spec[data['id']] = data
 
